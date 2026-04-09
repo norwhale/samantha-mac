da2fa8025b6c5e8f71d08d08c63f82fa8edbd03d
Yuichi Kujirai <uni.itcircle20555@gmail.com>
2026-04-09 18:03:38 +0300

    Initial Commit

---
+++ Samantha/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Samantha
+//
+//  Created by 鯨井優一 on 2026/04/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Samantha/SamanthaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SamanthaApp.swift
+//  Samantha
+//
+//  Created by 鯨井優一 on 2026/04/09.
+//
+
+import SwiftUI
+
+@main
+struct SamanthaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
